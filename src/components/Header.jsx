2adca36958c8a152c395b877a9715d05b0722522
Your Name <you@example.com>
2025-02-05 12:48:23 +0530

    add fontfamily

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
 
       <div className="container max-w-[1140px] mx-auto px-4 flex justify-between items-center ">
 
-        <a href="/" className="text-white font-normal text-[21.64px] leading-[113%] z-10 pt-7 pb-[30px] ">
+        <a href="/" className="text-white font-normal text-[21.64px] leading-[113%] z-10 pt-7 pb-[30px] ff-franklin">
           NEKOZUMA
         </a>
 
@@ -45,15 +45,15 @@ const Header = () => {
               <a
                 onClick={handleClick}
                 href={obj.link}
-                className="text-white font-normal  text-base leading-[130%] transition-transform delay-150 duration-300 ease-in-out hover:scale-150 inline-block link-hover"
+                className="text-white font-normal  text-base leading-[130%] transition-transform delay-150 duration-300 ease-in-out hover:scale-110 inline-block link-hover "
               >
                 {obj.title}
               </a>
             </li>
           ))}
           <ul className='flex gap-[18px] justify-center items-center'>
-            <li className='flex-shrink-0'><a className='transition-transform delay-150 duration-300 ease-in-out hover:scale-150 inline-block' href=""><img src={discord} alt="discord" /></a></li>
-            <li className='flex-shrink-0'><a className='transition-transform delay-150 duration-300 ease-in-out hover:scale-150 inline-block' href=""><img src={twitter} alt="twitter" /></a></li>
+            <li className='flex-shrink-0'><a onClick={handleClick} className='transition-transform delay-150 duration-300 ease-in-out hover:scale-120 inline-block' href="/discord"><img src={discord} alt="discord" /></a></li>
+            <li className='flex-shrink-0'><a onClick={handleClick} className='transition-transform delay-150 duration-300 ease-in-out hover:scale-120 inline-block' href="/twitter"><img src={twitter} alt="twitter" /></a></li>
           </ul>
         </ul>
 
@@ -79,27 +79,25 @@ const Header = () => {
       <div className='flex-grow w-full flex justify-center items-center'>
         <div className="container max-w-[1140px] mx-auto px-3  z-[5]">
           <div className="flex flex-wrap  flex-row px-[-12px] items-center">
-            
             <div className='w-full lg:w-1/2 px-3 mt-[50px] lg:mt-0'>
               <div className='flex justify-center items-center  lg:items-start flex-col'>
-              <h1 className='text-[#3586FF] font-normal text-[45px] text-center lg:text-start  md:text-[64px] leading-[113%] mb-[11px] text-main '>NEKOZUMA
-              </h1>
-              
-              <p className='font-normal text-base leading-[130%] text-[#D4E5FF] mb-[43px] text-center lg:text-start max-w-[502px]'>The collection  of 5555 NFTs on the Solana blockchain. The collection serves to be a rising force against the anime meta of collections with undoxxed teams and is therefore backed by the team at Nubbies NFT & Karmic Labs.</p>
-              <button className='font-normal text-base leading-[130%] text-white py-2 px-6 rounded-[9px] bg-[#245AAB] transition-transform delay-150 duration-300 ease-in-out hover:scale-120'>Connect Wallet</button>
+                <h1 className='text-[#3586FF] font-normal text-[45px] text-center lg:text-start  md:text-[64px] leading-[113%] mb-[11px] text-main ff-franklin '>NEKOZUMA
+                </h1>
+
+                <p className='font-normal text-base leading-[130%] text-[#D4E5FF] mb-[43px] text-center lg:text-start max-w-[502px]'>The collection  of 5555 NFTs on the Solana blockchain. The collection serves to be a rising force against the anime meta of collections with undoxxed teams and is therefore backed by the team at Nubbies NFT & Karmic Labs.</p>
+                <button className='font-normal text-base leading-[130%] text-white py-2 px-6 rounded-[9px] bg-[#245AAB] transition-all  duration-500 ease-in-out hover:scale-110 hover:bg-white hover:text-[#245aab]'>Connect Wallet</button>
               </div>
             </div>
-
             <div className='w-full lg:w-1/2 px-3 mt-6 lg:mt-0 '>
               <div className='lg:absolute bottom-0 right-0 '>
                 <div className='relative flex justify-center items-center'>
                   <img className=' hidden lg:block' src={bottom_layer} alt="layer" />
                   <div className='lg:absolute right-0 bottom-0 max-w-[564px] '>
                     <img className='w-full' src={brown_fox} alt="fox" /></div>
-              </div>
+                </div>
               </div>
             </div>
-            
+
           </div>
         </div>
       </div>
